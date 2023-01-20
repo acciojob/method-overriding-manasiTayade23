aeff1ffcc2d13653a2948a5cbb5dbabd40021db9
Manasi <mansitayade23@gmail.com>
2023-01-20 23:01:21 +0530

    Solution 2

--- src/main/java/com/driver/A.java
+++ src/main/java/com/driver/A.java
@@ -7,13 +7,14 @@ public class A {
 }
 class B extends A{
     public String meth(){
-       
+
         return "Method is overridden in Extendend class B";
 
     }
 
     public static void main(String[] args) {
         B obj=new B();
+        obj.meth();
         obj.meth();
     }
 }
